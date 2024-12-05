2ee0b124bbdc3be40134254eba54e79f8e39f61c
Eva Pace <eba.pachi@gmail.com>
2024-12-05 06:04:29 -0300

    day2: split part1 parsing from "logic"

--- day2/part1.c
+++ day2/part1.c
@@ -24,40 +24,39 @@ int main(void) {
     u32 buf_len = MAX_LINE_SIZE;
     char line[buf_len];
     int atoi_ret;
-    u8 numbers[8], i;
+    u8 numbers[8], i, len;
     u16 safe_count = 0;
     bool inc;
 
     while(fgets(line, buf_len, file)) {
-        i = 0;
-        for (char *p = strtok(line, " "); p != NULL; p = strtok(NULL, " ")) {
-            if ((atoi_ret = atoi(p))) {
-                numbers[i] = atoi_ret;
-                if (i > 0) {
-                    if (numbers[i-1] > numbers[i]) {
-                        if (i == 1) {
-                            inc = false;
-                        } else if (i > 1 && inc) {
-                            goto next;
-                        }
-                        if (numbers[i-1] - numbers[i] > 3)
-                            goto next;
-                    } else if (numbers[i] > numbers[i-1]) {
-                        if (i == 1) {
-                            inc = true;
-                        } else if (i > 1 && !inc) {
-                            goto next;
-                        }
-                        if (numbers[i] - numbers[i-1] > 3)
-                            goto next;
-                    } else {
-                        goto next;
-                    }
+        len = 0;
+        for (char *p = strtok(line, " "); p != NULL; p = strtok(NULL, " "))
+            if ((atoi_ret = atoi(p)))
+                numbers[len++] = atoi_ret;
+
+        for (i = 1; i < len; i++) {
+            if (numbers[i-1] > numbers[i]) {
+                if (i == 1) {
+                    inc = false;
+                } else if (i > 1 && inc) {
+                    goto next;
                 }
-                i++;
+                if (numbers[i-1] - numbers[i] > 3)
+                    goto next;
+            } else if (numbers[i] > numbers[i-1]) {
+                if (i == 1) {
+                    inc = true;
+                } else if (i > 1 && !inc) {
+                    goto next;
+                }
+                if (numbers[i] - numbers[i-1] > 3)
+                    goto next;
+            } else {
+                goto next;
             }
         }
-        if (i != 0)
+
+        if (len != 0)
             safe_count += 1;
 next:
         do {} while(0);
